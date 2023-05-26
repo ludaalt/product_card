b3dca105a5a1828bd2eb55bf53b8fe333247272b
Людмила Алтухова <luda.tamb@mail.ru>
2023-05-26 16:22:57 +0300

    feat: add product components

--- components/CardItem.tsx
+++ components/CardItem.tsx
@@ -1,5 +1,42 @@
-const CardItem = () => {
-  return <div></div>;
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/Cards.module.scss";
+import { formatPrice } from "../services/formatPrice";
+import { ICardItem } from "../types/types";
+
+import ColorPicker from "./ColorPicker";
+import Button from "./Button";
+
+type Props = {
+  item: ICardItem;
+};
+
+const CardItem: FC<Props> = ({ item }) => {
+  return (
+    <div className={styles.cardItem}>
+      <Image
+        src={`/product-${item.id}.png`}
+        alt={`Product ${item.id}`}
+        width={258}
+        height={258}
+      />
+      <div className={styles.cardPrice}>{formatPrice(item.price)}</div>
+      <div className={styles.cardTitle}>{item.title}</div>
+      <div className={styles.cardDescription}>{item.description}</div>
+      <ColorPicker colors={item.availableColors} />
+      <Button className={`${styles.button} ${styles.mainButton}`}>
+        Add to cart
+      </Button>
+      <Button className={`${styles.button} ${styles.secondaryButton}`}>
+        Buy now
+      </Button>
+      <Link className={styles.reviewsLink} href="#">
+        Read reviews
+      </Link>
+    </div>
+  );
 };
 
 export default CardItem;
--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,7 +1,19 @@
+import { data } from "../data";
+import CardItem from "./CardItem";
+
+import styles from "../styles/Cards.module.scss";
+import { ICardItem } from "../types/types";
+
 const CardList = () => {
-  return <div>Card List</div>;
+  return (
+    <ul className={styles.cardList}>
+      {data.map((item: ICardItem) => (
+        <li key={item.id}>
+          <CardItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default CardList;
-
-// declare global { namespace JSX { ... ... ... } }
